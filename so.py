62f3acb311eed91c634316b1999f1c047643c1c1
padd60 <padd60@naver.com>
2021-10-21 23:56:12 +0900

    Extract job list to CSV file

--- so.py
+++ so.py
@@ -27,7 +27,7 @@ def extract_job(html):
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
-        print(f"Srapping SO: Page: {page}")
+        print(f"Scrapping SO: Page: {page}")
         result = requests.get(f'{URL}&pg={page + 1}')
         soup = BeautifulSoup(result.text, 'html.parser')
         results = soup.find_all('div', {'class': '-job'})
